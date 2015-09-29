0becddc0ee76ae088bda2020e282fbf0933f35f8
terminator10 <bolhy10@hotmail.com>
2015-09-29 01:55:40 -0500

    Me falto subir una imagen

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,53 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+function evaluador(){
+    var miv = $('.vlor');
+    var img = document.getElementById('resp');
+    var c = 0;
+    for(var i=0;i<miv.length;i++)
+    {
+        if(miv[i].checked)
+            resultado=miv[i].value;
+    }
+
+    var cont = 0;
+    for (var x=0; x < miv.length; x++) {
+        if (miv[x].checked){
+            r = miv[x].value;
+            if(r == 1 || r == 2 || r == 3 || r == 4 || r == 5){
+                cont = cont + 1;
+            }else{
+                cont = cont;
+            }
+
+        }
+
+    }
+    if(resultado == 1){
+        img.src="img/smiley-150841_1280.png";
+        $('#bienvenida').html("Que bien. Prestaste Atención. Me alegro").show();
+        $('.contador').html(cont+' <i class="glyphicon glyphicon-ok text-success"></i>');
+    }else if(resultado == 2){
+        img.src="img/smiley-150841_1280.png";
+        $('#bienvenida').html("Que bien vas por la segunda . Me alegro").show();
+        $('.contador').html(cont+' <i class="glyphicon glyphicon-ok text-success"></i>');
+    }else if(resultado == 3){
+        img.src="img/smiley-150841_1280.png";
+        $('#bienvenida').html("Ooh! .Bien hecho. Me alegro").show();
+        $('.contador').html(cont+' <i class="glyphicon glyphicon-ok text-success"></i>');
+    }else if(resultado == 4){
+        img.src="img/smiley-150841_1280.png";
+        $('#bienvenida').html("Interesante estuvo la pregunta. Bien hecho. Me alegro").show();
+        $('.contador').html(cont+' <i class="glyphicon glyphicon-ok text-success"></i>');
+    }else if(resultado == 5){
+        img.src="img/smiley-150841_1280.png";
+        $('#bienvenida').html("No tengo nada que decir. Gracias por jugar conmigo").show();
+        $('.contador').html(cont+' <i class="glyphicon glyphicon-ok text-success"></i>');
+    }else{
+        img.src="img/smiley-150837_1280.png";
+        $('#bienvenida').html("Ooh! no Prestaste Atención. Que mal").show();
+    }
+
+}
